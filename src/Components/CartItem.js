10258c09586b24fed6c9154371bf131bdd8b704a
Anudeep <anudeepmarlapalli22@gmail.com>
2021-03-06 23:05:23 +0530

    Adding shopping Cart

---
+++ src/Components/CartItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Button
+} from "reactstrap";
+
+const CartItem = ({name, product, addInCart }) => {
+  return (
+    <Card className="mt-2 mb-1">
+      <CardImg top height="250" width="100%" src={product.smallImage} />
+      <CardBody className="text-center">
+        <CardTitle> Brand - {product.productName}</CardTitle>
+        <CardText className="secondary">
+          price: RS {product.productPrice}
+        </CardText>
+        <cardText>  Seller : {product.ownerName}  </cardText>
+        <br></br>
+        <Button color="success" onClick={() => addInCart(product)}>
+          Buy Now
+        </Button>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default CartItem;
