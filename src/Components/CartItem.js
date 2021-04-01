344d8358246aac41ab465abdc1ca2e6ef6348f31
Anudeep <anudeepmarlapalli22@gmail.com>
2021-04-01 23:53:35 +0530

    Fixing cart issue

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -15,11 +15,12 @@ const CartItem = ({name, product, addInCart }) => {
     <Card className="mt-2 mb-1">
       <CardImg top height="250" width="100%" src={product.smallImage} />
       <CardBody className="text-center">
-        <CardTitle> Brand - {product.productName}</CardTitle>
+        <CardTitle> Brand - wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</CardTitle>
         <CardText className="secondary">
           price: RS {product.productPrice}
         </CardText>
         <cardText>  Seller : {product.ownerName}  </cardText>
+        <br></br>
         <br></br>
         <Button color="success" onClick={() => addInCart(product)}>
           Buy Now
